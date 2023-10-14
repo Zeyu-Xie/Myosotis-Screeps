074e87ff3337e199b2ee4896e4cbbef0b2029147
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2023-10-14 15:19:50 +0800

    Add files via upload

--- role.harvester.js
+++ role.harvester.js
@@ -1,24 +1,14 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.harvester');
- * mod.thing == 'a thing'; // true
- */
-
 var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        
 	    if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
-        else {
+        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['Spawn1']);
             }
