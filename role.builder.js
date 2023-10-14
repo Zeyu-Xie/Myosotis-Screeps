f054ed8f0d2620cb4988a3fbcc498fcb915c397f
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2023-10-14 16:01:22 +0800

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.builder');
- * mod.thing == 'a thing'; // true
- */
-
 var roleBuilder = {
 
     /** @param {Creep} creep **/
