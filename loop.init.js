2ff7aeda4986899aab12d7ca7d74c05a489fcee1
Acan <xie.zeyu20@gmail.com>
2023-10-16 16:24:42 +0800

    .

--- loop.init.js
+++ loop.init.js
@@ -8,16 +8,19 @@
  */
 
 var loopInit = function () {
+
     console.log("========")
-    console.log(Date())
+    console.log("* Clock: " + Date())
+    console.log("* Tick: " + Game.time)
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-
-    printCreepsNum()
+    printStorageInfo()
+    printRclInfo()
+    printCreepsInfo()
 
     console.log("--------")
     console.log("Loop Starts")
@@ -25,7 +28,7 @@ var loopInit = function () {
 
 module.exports = loopInit
 
-var printCreepsNum = function () {
+var printCreepsInfo = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
@@ -48,6 +51,14 @@ var printCreepsNum = function () {
     l1 = (l1 + " |").substring(1)
     l2 = (l2 + " |").substring(1)
 
-    console.log("* Creep Numbers: "+l1)
-    console.log("                 "+l2)
+    console.log("* Creep Numbers: " + l1)
+    console.log("                 " + l2)
+}
+
+var printStorageInfo = function () {
+    console.log("* Storage: Energy: ", Game.spawns["Spawn1"].room.energyAvailable + "/" + Game.spawns["Spawn1"].room.energyCapacityAvailable)
+}
+
+var printRclInfo = function () {
+    console.log("* RCL: Lv: " + Game.spawns["Spawn1"].room.controller.level + ", Progress: " + Game.spawns["Spawn1"].room.controller.progress + "/" + Game.spawns["Spawn1"].room.controller.progressTotal)
 }
