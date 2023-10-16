34222e2b34ac8083a45fa0aaa8593f97ddd53184
acanxie <xie.zeyu20@gmail.com>
2023-10-16 17:47:39 +0800

    .

--- main.js
+++ main.js
@@ -8,6 +8,49 @@ var loopInit = require("loop.init")
 var loopEnd = require("loop.end");
 const slogan = require('slogan');
 
+// 初始化
+const roleMap = new Map()
+const roleList = ["harvester", "builder", "upgrader", "repairer", "healer"]
+const numList = [7, 7, 2, 3, 0]
+const bodyPartList = [
+    [
+        [WORK, CARRY, MOVE],
+        [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]
+    ],
+    [
+        [WORK, CARRY, MOVE],
+        [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]
+    ],
+    [
+        [WORK, CARRY, MOVE],
+        [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]
+    ],
+    [
+        [WORK, CARRY, MOVE],
+        [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]
+    ],
+    [
+        [HEAL, CARRY, MOVE],
+        [HEAL, HEAL, HEAL, HEAL, CARRY, MOVE, MOVE]
+    ]
+]
+roleMap.set("harvester", 0)
+roleMap.set("builder", 1)
+roleMap.set("upgrader", 2)
+roleMap.set("repairer", 3)
+roleMap.set("healer", 4)
+
+var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == "repairer");
+var healers = _.filter(Game.creeps, (creep) => creep.memory.role == "healer");
+const roles = [harvesters, builders, upgraders, repairers, healers]
+
+var energyAvailable = Game.spawns["Spawn1"].room.energyAvailable
+var energyCapacityAvailable = Game.spawns["Spawn1"].room.energyCapacityAvailable
+
+
 module.exports.loop = function () {
 
     loopInit()
@@ -36,97 +79,39 @@ var nameStandardize = function () {
 
 var spawn = function () {
 
-    const roleMap = new Map()
-    const roleList = ["harvesters", "builders", "upgraders", "repairers", "healers"]
-    roleMap.set("harvesters", 0)
-    roleMap.set("builders", 1)
-    roleMap.set("upgraders", 2)
-    roleMap.set("repairers", 3)
-    roleMap.set("healers", 4)
-    console.log(roleMap[2])
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == "repairer");
-    var healers = _.filter(Game.creeps, (creep) => creep.memory.role == "healer");
-    const roles = [harvesters, builders, upgraders, repairers, healers]
-
-    var energyAvailable = Game.spawns["Spawn1"].room.energyAvailable
-    var energyCapacityAvailable = Game.spawns["Spawn1"].room.energyCapacityAvailable
-
-
-
-    if (harvesters.length < 7) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        if (energyCapacityAvailable < 550) {
-            Game.spawns['Spawn1'].spawnCreep([MOVE, CARRY, WORK], newName,
-                { memory: { role: 'harvester' } });
-        }
-        else Game.spawns['Spawn1'].spawnCreep([MOVE, CARRY, MOVE, WORK, WORK, WORK, WORK], newName,
-            { memory: { role: 'harvester' } });
-    }
-    else if (upgraders.length < 2) {
-        var newName = 'Upgrader' + Game.time;
-        console.log('Spawning new upgrader: ' + newName);
-        if (energyCapacityAvailable < 550) {
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-                { memory: { role: 'upgrader' } });
-        }
-        else {
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, WORK, WORK, WORK, MOVE], newName,
-                { memory: { role: 'upgrader' } });
-        }
-
-    }
-    else if (builders.length < 7) {
-        var newName = 'Builder' + Game.time;
-        console.log('Spawning new builder: ' + newName);
-        if (energyCapacityAvailable < 550) {
-            Game.spawns['Spawn1'].spawnCreep([MOVE, CARRY, WORK], newName,
-                { memory: { role: 'builder' } });
-        }
-        else {
-            Game.spawns['Spawn1'].spawnCreep([MOVE, CARRY, MOVE, WORK, WORK, WORK, WORK], newName,
-                { memory: { role: 'builder' } });
-        }
-    }
-    else if (repairers.length < 3) {
-        var newName = 'Repairer' + Game.time;
-        console.log('Spawning new repairer: ' + newName);
-        if (energyCapacityAvailable < 550) {
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-                { memory: { role: 'repairer' } });
-        }
-        else {
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, WORK, WORK, WORK, MOVE], newName,
-                { memory: { role: 'repairer' } });
-        }
-    }
-    else if (healers.length < 0) {
-        var newName = 'Healer' + Game.time;
-        console.log('Spawning new healer: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([HEAL, CARRY, MOVE], newName,
-            { memory: { role: 'healer' } });
-    }
-
-    if (Game.spawns['Spawn1'].spawning) {
+    // 判断是否已在 Spawning
+    if (Game.spawns["Spawn1"].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
             '🛠️' + spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1,
             Game.spawns['Spawn1'].pos.y,
             { align: 'left', opacity: 0.8 });
+        return
     }
 
-    if (!Game.spawns["Spawn1"].spawning) {
-        const s = slogan("POLITICS", 0.1)
-        if (s.length >= 1) {
-            Game.spawns['Spawn1'].room.visual.text(
-                s,
-                Game.spawns['Spawn1'].pos.x + 1,
-                Game.spawns['Spawn1'].pos.y,
-                { align: 'left', opacity: 0.8 });
+    for (let i = 0; i < roleList.length; i++) {
+
+        // 判断数量是否足够
+        if (Game.spawns["Spawn1"].room.find(FIND_MY_CREEPS, {
+            filter: (creep) => {
+                return creep.memory.role == roleList[i]
+            }
+        }).length >= numList[i]) {
+            continue
+        }
+
+        // 命名
+        var name = roleList[i][0].toUpperCase() + roleList[i].substring(1) + Game.time
+
+        // 判断生产“体型”
+        if (energyAvailable < 550) {
+            Game.spawns['Spawn1'].spawnCreep(bodyPartList[i][0], name,
+                { memory: { role: roleList[i] } });
+        }
+        else {
+            Game.spawns['Spawn1'].spawnCreep(bodyPartList[i][1], name,
+                { memory: { role: roleList[i] } });
         }
     }
 }
