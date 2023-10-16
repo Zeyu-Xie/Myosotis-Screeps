38a7a7c87fed997bde839fb681046e9430b48f6f
Acan <xie.zeyu20@gmail.com>
2023-10-16 15:33:46 +0800

    .

--- loop.init.js
+++ loop.init.js
@@ -10,25 +10,44 @@
 var loopInit = function () {
     console.log("========")
     console.log(Date())
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
+
+    printCreepsNum()
+
+    console.log("--------")
+    console.log("Loop Starts")
+}
+
+module.exports = loopInit
+
+var printCreepsNum = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == "repairer");
     var healers = _.filter(Game.creeps, (creep) => creep.memory.role == "healer");
-    console.log('Harvesters: ' + harvesters.length);
-    console.log('Upgraders: ' + upgraders.length);
-    console.log('Builders: ' + builders.length);
-    console.log('Repairers: ' + repairers.length);
-    console.log('Healers: ' + healers.length);
-    console.log("--------")
-    console.log("Loop Starts")
-}
 
-module.exports = loopInit
+    const strArray = ["harvesters", "upgraders", "builders", "repairers", "healers"]
+    const numArray = [harvesters.length, upgraders.length, builders.length, repairers.length, healers.length]
+    const num = strArray.length
+
+    var l1 = ""
+    var l2 = ""
+
+    for (var i = 0; i < num; i++) {
+        l1 = l1 + " |" + strArray[i].padStart(12, " ")
+        l2 = l2 + " |" + String(numArray[i]).padStart(12, " ")
+    }
+
+    l1 = (l1 + " |").substring(1)
+    l2 = (l2 + " |").substring(1)
+
+    console.log("* Creep Numbers: "+l1)
+    console.log("                 "+l2)
+}
