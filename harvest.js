bb601e8c76b058eb24815bcb8cb2ac8de6ddbacd
acanxie <xie.zeyu20@gmail.com>
2023-10-17 15:53:08 +0800

    .

--- harvest.js
+++ harvest.js
@@ -9,15 +9,21 @@ var harvest = (creep) => {
         }
     }
 
-    // role 不为 harvester：从 container 中获取能量
+    // role 不为 harvester：从 container 中获取能量，若所有的 container 里面均无能量，则从 resource 中获取能量
     else {
         var containerSources = creep.room.find(FIND_STRUCTURES, {
             filter: { structureType: STRUCTURE_CONTAINER }
         })
+        var sources = creep.room.find(FIND_SOURCES);
         if (creep.withdraw(containerSources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(containerSources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
             return
         }
+        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+            return
+        }
+
     }
 }
 
