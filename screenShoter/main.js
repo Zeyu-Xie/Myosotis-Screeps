c3981ee566ff62a3b000ebec6df979db6db83e71
Acan <xie.zeyu20@gmail.com>
2023-10-18 15:05:02 +0800

    .

--- screenShoter/main.js
+++ screenShoter/main.js
@@ -4,13 +4,71 @@ let once = true
 let map = Array.from({ length: 50 }, () => Array(50).fill(0));
 
 module.exports.loop = function () {
+    if (!once) return
     const initData = init()
-    for(var i=0;i<50;i++) {
-        for(var j=0;j<50;j++) {
-            const targetPosition = new RoomPosition(i,j,initData.roomName)
-            const structure = initData.room.lookAt(i,j)
-            map[i][j] = structure[0].type
+    for (var i = 0; i < 50; i++) {
+        for (var j = 0; j < 50; j++) {
+            const game = initData.room.lookAt(i, j)
+            map[i][j] = simplify(game[0])
+
         }
     }
-    console.log(map)
+    printMap()
+    once = false
+}
+
+const printMap = () => {
+    for(let i=0;i<50;i++) {
+        let str = ""
+        for(let j=0;j<50;j++) {
+            str += map[j][i]
+        }
+        console.log(`${i}`.padStart(2, " "), str)
+    }
+}
+
+const printMapTranspose = () => {
+    for(let i=0;i<50;i++) {
+        let str = ""
+        for(let j=0;j<50;j++) {
+            str += map[i][j]
+        }
+        console.log(`${i}`.padStart(2, " "), str)
+    }
+}
+
+const simplify = function (point) {
+    if(point.type == "structure") {
+        switch(point.structure.structureType) {
+            case "spawn": {
+                return " S"
+            }
+            case "controller": {
+                return " C"
+            }
+            default: {
+                return " ?"
+            }
+        }
+    }
+    else if (point.type == "creep") {
+        return " o"
+    }
+    else if(point.type == "terrain") {
+        switch (point.terrain) {
+            case "plain": {
+                return " ."
+            }
+            case "wall": {
+                return "[]"
+            }
+            case "swamp": {
+                return "__"
+            }
+            default: {
+                return " ."
+            }
+        }
+    }
+    else return "  "
 }
